d8302e22c3c3738e3fbe2ac01359eede7ef781f8
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-09-28 18:21:08 -0400

    Bug fix. Change nav bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,9 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">' 
+					// + $('#logo').html() 
+					+ '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
