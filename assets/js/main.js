d7c804c94d312c3dc80c269e31dd1b273a2e406f
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-09-29 17:53:35 -0400

    Add nav bar feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
 					'<span class="title">' 
-					// + $('#logo').html() 
+					+ $('#logo').html() 
 					+ '</span>' +
 				'</div>'
 			)
